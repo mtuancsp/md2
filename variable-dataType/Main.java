4658625cc912fe85ded38f0a5ac7a6df23d0a704
mtuancsp <minhtuan267@gmail.com>
2023-03-22 10:36:11 +0700

    ;

--- variable-dataType/Main.java
+++ variable-dataType/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + i);
+        System.out.println("d = " + i);
+        System.out.println("b = " + i);
+        System.out.println("c = " + i);
+        System.out.println("s = " + i);
+
+    }
 }
