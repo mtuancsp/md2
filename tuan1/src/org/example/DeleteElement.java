40f626d0db111958cb9ebeda7afe6b99a1b01cf5
mtuancsp <minhtuan267@gmail.com>
2023-03-23 21:54:31 +0700

    ;

--- tuan1/src/org/example/DeleteElement.java
+++ tuan1/src/org/example/DeleteElement.java
@@ -1,2 +1,31 @@
-package org.example;public class DeleteElement {
+package org.example;
+
+public class DeleteElement {
+    public static int[] deleteElements(int[] arr, int element) {
+        int count = 0;
+        for (int e : arr) {
+            if (e == element) {
+                count++;
+            }in
+        }
+        if (count == 0) {
+            System.out.println("Element not found in array");
+        }
+        int[] newArr = new int[arr.length - count];
+        int j = 0;
+        for (int e : arr) {
+            if (e != element) {
+                newArr[j++] = e;
+            }
+        }
+        return newArr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 5, 7, 9, 5, 10};
+        int element = 5;
+        for (int e : deleteElements(arr, element)) {
+            System.out.print(e + " ");
+        }
+    }
 }
