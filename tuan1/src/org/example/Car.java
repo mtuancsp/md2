e91b2f39b184932ef20f30b24daad2c3407ae7ca
mtuancsp <minhtuan267@gmail.com>
2023-03-26 14:26:03 +0700

    ;

---
+++ tuan1/src/org/example/Car.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Car {
+    private String name;
+    private String engine;
+
+    public static int numberOfCars = 0;
+
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+
+}
