3c02f19e0d085aacd7047173a8b285cbf2e7603a
mtuancsp <minhtuan267@gmail.com>
2023-03-28 22:51:35 +0700

    ;

---
+++ tuan1/src/org/example/TestComparableCircle.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Arrays;
+
+
+public class TestComparableCircle {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6, "blue", false);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5, "red", true);
+        System.out.println(circles[0].compareTo(circles[1]));
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle c : circles) {
+            System.out.println(c);
+        }
+        Arrays.sort(circles);
+        System.out.println("Sorted:");
+        for (ComparableCircle c : circles) {
+            System.out.println(c);
+        }
+    }
+}
+class ComparableCircle extends Circles implements Comparable<ComparableCircle> {
+    public ComparableCircle() {
+
+    }
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+    public ComparableCircle(double radius, String color, boolean filled ) {
+        super(color, filled, radius);
+}
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) {
+            return 1;
+        } else if (getRadius() < o.getRadius()) {
+            return -1;
+        }
+        return 0;
+    }
+}
