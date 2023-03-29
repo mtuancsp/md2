c412e253f68b32dd04512fc37e35b8a79488cb42
mtuancsp <minhtuan267@gmail.com>
2023-03-29 14:45:45 +0700

    ;

---
+++ tuan1/src/p8/CalculatorTest.java
@@ -0,0 +1,10 @@
+package p8;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        System.out.println(Calculator.calculate(1, 2, '+'));
+        System.out.println(Calculator.calculate(1, 2, '-'));
+        System.out.println(Calculator.calculate(1, 2, '*'));
+        System.out.println(Calculator.calculate(1, 2, '/'));
+    }
+}
