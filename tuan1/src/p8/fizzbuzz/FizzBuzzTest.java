837b7398411e2a71fd6943963cf17faad82aa69d
mtuancsp <minhtuan267@gmail.com>
2023-03-29 14:55:05 +0700

    ;

---
+++ tuan1/src/p8/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+package p8.fizzbuzz;
+
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(FizzBuzz.fizzBuzz(i));
+        }
+    }
+}
