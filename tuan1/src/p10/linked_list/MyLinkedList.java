25a9dfa18d86919deeace154cdfe632a262738b2
mtuancsp <minhtuan267@gmail.com>
2023-04-01 11:24:24 +0700

    a

--- tuan1/src/p10/linked_list/MyLinkedList.java
+++ tuan1/src/p10/linked_list/MyLinkedList.java
@@ -12,25 +12,42 @@ public class MyLinkedList {
         this.tail = null;
     }
 
+    public String toString() {
+        String result = "MyLinkedList: ";
+        for (int i = 0; i < size; i++) {
+            result += get(i) + " ";
+        }
+        return result;
+    }
+
+    private void checkIndex(int index) {
+        if(index < 0 || index > size){
+            throw new IndexOutOfBoundsException( "Index: " + index + ", Size: " + size);
+        }
+    }
+
     //add(int index, Object data)
     public void add(int index, Object data) {
-        Node newNode = new Node(data);
+        checkIndex(index);
         if (index == 0) {
             addFirst(data);
         } else if (index == size) {
             addLast(data);
         } else {
             Node current = head;
+            Node newNode = new Node(data);
             for (int i = 0; i < index - 1; i++) {
                 current = current.next;
             }
             newNode.next = current.next;
             current.next = newNode;
+            size++;
         }
-        size++;
+
     }
     //get(int index)
     public Object get(int index) {
+        checkIndex(index);
         Node current = head;
         for (int i = 0; i < index; i++) {
             current = current.next;
@@ -42,13 +59,13 @@ public class MyLinkedList {
     public void addFirst(Object data) {
         Node newNode = new Node(data);
         if (head == null) {
-            head = newNode;
             tail = newNode;
         } else {
             newNode.next = head;
-            head = newNode;
         }
+        head = newNode;
         size++;
+
     }
     public Object getFirst() {
         if (head == null) {
@@ -74,6 +91,114 @@ public class MyLinkedList {
             return tail.data;
         }
     }
+
+    //remove( int index)
+    public void remove(int index) {
+        checkIndex(index);
+        if (index == 0) {
+            removeFirst();
+        } else if (index == size - 1) {
+            removeLast();
+        } else {
+            Node current = head;
+            for (int i = 0; i < index -1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+            size--;
+        }
+
+    }
+    //remove(Object data)
+    public boolean remove(Object data) {
+        if (head == null) {
+            return false;
+        } else if (head.data.equals(data)) {
+            removeFirst();
+            return true;
+        } else if (tail.data.equals(data)) {
+            removeLast();
+            return true;
+        } else {
+            Node current = head.next;
+            Node previous = head;
+            while (current != null) {
+                if (current.data.equals(data)) {
+                    previous.next = current.next;
+                    size--;
+                    return true;
+                }
+                previous = current;
+                current = current.next;
+            }
+            return false;
+        }
+    }
+
+    public Object removeFirst() {
+        if (head == null) {
+            return null;
+        } else {
+            Object data = head.data;
+            head = head.next;
+            size--;
+            return data;
+        }
+    }
+    public Object removeLast() {
+        if (head == null) {
+            return null;
+        } else {
+            Object data = tail.data;
+            tail = tail.next;
+            size--;
+            return data;
+        }
+    }
+
+    //clone()
+    public MyLinkedList clone() {
+        MyLinkedList clone = new MyLinkedList();
+        Node current = head;
+        while (current!= null) {
+            clone.addLast(current.data);
+            current = current.next;
+        }
+        return clone;
+    }
+
+    //contains()
+    public boolean contains(Object data) {
+        for (int i = 0; i < size; i++) {
+            if (get(i).equals(data)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //indexOf()
+    public int indexOf(Object data) {
+        for (int i = 0; i < size; i++) {
+            if (get(i).equals(data)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //isEmpty()
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //clear()
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
 }
 
 
--- tuan1/src/p10/linked_list/MyLinkedListTest.java
+++ tuan1/src/p10/linked_list/MyLinkedListTest.java
@@ -3,15 +3,54 @@ package p10.linked_list;
 public class MyLinkedListTest {
 
     public static void main(String[] args) {
-    MyLinkedList myLinkedList = new MyLinkedList();
-    myLinkedList.addFirst("a");
-    myLinkedList.addFirst("b");
-    myLinkedList.addFirst("c");
-    myLinkedList.addLast("d");
-    myLinkedList.add(1, "e");
+        MyLinkedList list = new MyLinkedList();
 
-    System.out.println(myLinkedList.get(10));
+        list.add(0, "a");
+        list.add(1, "b");
+        list.add(2, "c");
+        list.add(3, "d");
+        System.out.println(list); // MyLinkedList: a b c d
 
+        // Test get()
+        System.out.println(list.get(0)); // a
+        System.out.println(list.get(1)); // b
+        System.out.println(list.get(2)); // c
+        System.out.println(list.get(3)); // d
+
+        // Test addFirst(), addLast(), getFirst(), getLast()
+        list.addFirst("z");
+        list.addLast("e");
+        System.out.println(list); // MyLinkedList: z a b c d e
+        System.out.println(list.getFirst()); // z
+        System.out.println(list.getLast()); // e
+
+        // Test remove(), removeFirst(), removeLast()
+        list.remove(2);
+        System.out.println(list.toString()); // MyLinkedList: z a c d e
+        System.out.println(list.removeFirst()); // z
+        System.out.println(list.toString()); // MyLinkedList: a c d e
+        System.out.println(list.removeLast()); // e
+        System.out.println(list.toString()); // MyLinkedList: a c d
+
+        // Test clone()
+        MyLinkedList list2 = list.clone();
+        System.out.println(list2.toString()); // MyLinkedList: a c d
+        list2.addLast("e");
+        System.out.println(list.toString()); // MyLinkedList: a c d
+        System.out.println(list2.toString()); // MyLinkedList: a c d e
+
+        // Test contains() and indexOf()
+        System.out.println(list.contains("a")); // true
+        System.out.println(list.contains("b")); // false
+        System.out.println(list.indexOf("a")); // 0
+        System.out.println(list.indexOf("b")); // -1
+
+        // Test isEmpty() and clear()
+        System.out.println(list.isEmpty()); // false
+        list.clear();
+        System.out.println(list.isEmpty()); // true
 
     }
+
 }
+
