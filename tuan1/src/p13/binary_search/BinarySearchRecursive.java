ab262f2848500071101199e9f2b3628f6d68a905
mtuancsp <minhtuan267@gmail.com>
2023-04-05 21:02:50 +0700

    james

---
+++ tuan1/src/p13/binary_search/BinarySearchRecursive.java
@@ -0,0 +1,28 @@
+package p13.binary_search;
+
+import java.util.Arrays;
+
+public class BinarySearchRecursive {
+    public static void main(String[] args) {
+        int[] arr = {10, 2, 3, 14, 20, 40, 75, 60, 7};
+        int value = 40;
+        Arrays.sort(arr);
+        int index = binarySearchRecursive(arr, 0, arr.length - 1, value);
+        System.out.println(index);
+    }
+
+    public static int binarySearchRecursive(int[] arr, int left, int right, int value) {
+
+        if (left > right) return -1;
+
+        int middle = (left + right) / 2;
+
+        if (arr[middle] == value) return middle;
+
+        if (arr[middle] < value) return binarySearchRecursive(arr, middle + 1, right, value);
+
+        return binarySearchRecursive(arr, left, middle - 1, value);
+    }
+
+}
+
