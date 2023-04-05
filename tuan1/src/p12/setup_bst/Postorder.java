37b432e0dcd5dafddd8bb3b5379eabf3a888c5de
mtuancsp <minhtuan267@gmail.com>
2023-04-05 08:11:47 +0700

    james

--- tuan1/src/p12/setup_bst/Postorder.java
+++ tuan1/src/p12/setup_bst/Postorder.java
@@ -3,10 +3,10 @@ package p12.setup_bst;
 public class Postorder {
     public static void main(String[] args) {
         BST<Integer> tree = new BST<>();
-        tree.insert(10);
+        tree.insert(27);
         tree.insert(19);
         tree.insert(14);
-        tree.insert(27);
+        tree.insert(10);
         tree.insert(35);
         tree.insert(42);
         tree.insert(31);
