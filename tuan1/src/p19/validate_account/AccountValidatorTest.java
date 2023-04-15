53be7cac1fd1066411e87af148911897890b76fa
mtuancsp <minhtuan267@gmail.com>
2023-04-15 15:27:44 +0700

    james

--- tuan1/src/p19/validate_account/AccountValidatorTest.java
+++ tuan1/src/p19/validate_account/AccountValidatorTest.java
@@ -4,7 +4,7 @@ public class AccountValidatorTest {
     public static final String[] validAccount = new String[]{"123abc_", "_abc123", "______", "123456", "abcdefgh"};
     public static final String[] invalidAccount = new String[]{".@", "12345", "1234_", "abcde"};
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         var accountValidator = new AccountValidator();
         for (String account : validAccount) System.out.println("Account is " + account + " is valid: " + accountValidator.isValid(account));
         for (String account : invalidAccount) System.out.println("Account is " + account + " is valid: " + accountValidator.isValid(account));
