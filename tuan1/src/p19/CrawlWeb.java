52469cdabdb6850c97d0e76643cbe2bc3726aa66
mtuancsp <minhtuan267@gmail.com>
2023-04-16 22:18:45 +0700

    james

---
+++ tuan1/src/p19/CrawlWeb.java
@@ -0,0 +1,32 @@
+package p19;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlWeb {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
+            // open the stream and put it into BufferedReader
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            // close scanner
+            scanner.close();
+            // remove all new line
+            content = content.replaceAll("\\n+", "");
+            // regex
+            Pattern p = Pattern.compile("alt=\"(.*?)\" data-src=");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ tuan1/src/p19/Test.java
@@ -0,0 +1,10 @@
+package p19;
+
+public class Test {
+    public static void main(String[] args) {
+        String x = "abc";
+        String y = "abc";
+        x.concat(y);
+        System.out.print(x);
+    }
+}
